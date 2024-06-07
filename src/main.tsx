9d1984b45d68739a0fc32720513eeea29e24b2f9
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-06-07 11:19:47 -0300

    move main toast container

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import { appTheme } from "./styles/theme.ts";
 import { GlobalStyles } from "./styles/global.ts";
 import { AuthProvider } from "./contexts/authContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { AppRoutes } from "./routes/index.tsx";
 import { TaskProvider } from "./contexts/taskContext.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -18,17 +18,18 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <ThemeProvider theme={appTheme}>
             <AppRoutes />
             <GlobalStyles />
-            <ToastContainer
-              theme={"dark"}
-              autoClose={3000}
-              closeOnClick={true}
-              pauseOnHover={true}
-              position={"top-right"}
-              pauseOnFocusLoss={false}
-            />
           </ThemeProvider>
         </TaskProvider>
       </AuthProvider>
     </QueryClientProvider>
+
+    <ToastContainer
+      theme={"dark"}
+      autoClose={3000}
+      closeOnClick={true}
+      pauseOnHover={true}
+      position={"top-right"}
+      pauseOnFocusLoss={false}
+    />
   </React.StrictMode>
 );
